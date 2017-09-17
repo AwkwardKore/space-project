e94114bf2edc701ea1ac6f73a5ad1ed5beeab3cc
Rolando Islas <dani.islas96@gmail.com>
2017-09-17 15:30:55 -0500

    Change folder structure

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../stylesheets/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        WELCOME
+      </div>
+    );
+  }
+}
+
+export default App;
